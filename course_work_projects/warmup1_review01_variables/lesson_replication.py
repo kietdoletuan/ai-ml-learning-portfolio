6be66de117f27b1664e6f8453b1aaa98021902a9
kietdoletuan <kietdoletuan@gmail.com>
2026-03-07 14:40:06 +0700

    refactor: refactored files directories into a more centralized structure around course lessons
    fix: redudant dataset references, typo in pizza chat bot, wrong directories as a result from refactoring for both visualization projects in warmpup1_review14

---
+++ course_work_projects/warmup1_review01_variables/lesson_replication.py
@@ -0,0 +1,9 @@
+#Lesson Date: 22/12
+print("Hello world!")
+
+raw_message  = "Have a nice day"
+message = "-".join(raw_message.split())
+print(message)
+
+message_1 = "Let's have a Tet holiday"
+print(message_1)
