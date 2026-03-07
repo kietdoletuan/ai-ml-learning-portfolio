8927c285686643bc04306dbe7870e1662c0d9348
kietdoletuan <kietdoletuan@gmail.com>
2026-03-07 14:51:51 +0700

    refactor: further cerntralization around course_lessons

--- course_work/warmup1_review01_variables/simple_message.py
+++
@@ -1,9 +0,0 @@
-#Lesson Date: 22/12
-print("Hello world!")
-
-raw_message  = "Have a nice day"
-message = "-".join(raw_message.split())
-print(message)
-
-message_1 = "Let's have a Tet holiday"
-print(message_1)
