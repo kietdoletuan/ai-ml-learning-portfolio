1db465cfa057ccaf7c4072bc5389badd757152e1
kietdoletuan <kietdoletuan@gmail.com>
2026-03-07 21:59:03 +0700

    add: added parents folders for lectures  folders to organize by phases of the course

--- course_work_projects/WarmUp1/warmup1_review02_functions/lesson_replication.py
+++ course_work_projects/WarmUp1/warmup1_review02_functions/lesson_replication.py
@@ -1,4 +1,5 @@
 #Lesson Date: 25/12
+import math
 
 status = True
 
@@ -16,4 +17,14 @@ while status != False:
     print(f"Square is {sqr(x)} and compare result is {compare(x)}")
 
 
+def sigmoid(x):
+    return 1 / 1 + (math.exp(-x))
+
+def tanh1(x):
+    return math.sinh(x)/math.cosh(x)
+
+def tanh2(x):
+    return (math.exp(x) - math.exp(-x))/(math.exp(x)+math.exp(-x))
+
+print(sigmoid(5), tanh1(6), tanh1(6))
 
